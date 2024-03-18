5117b9e707573df5de27e1b2497be2e3db9eb9e8
bpunyagunr <132117860+bpunyagunr@users.noreply.github.com>
2024-03-18 16:34:25 -0500

    Goal.java

--- src/main/java/oop/trials/fittracker/goal/Goal.java
+++ src/main/java/oop/trials/fittracker/goal/Goal.java
@@ -1,2 +1,43 @@
-package oop.trials.fittracker.goal;public class Goal {
+package oop.trials.fittracker.goal;
+import java.time.LocalDate;
+public class Goal implements Record{
+    private String goalType;
+    private double goalValue;
+    private double currentWeight;
+
+    public void setGoalType(String goalType){
+        this.goalType = goalType;
+    }
+    public void setValue(double goalValue){
+        this.goalValue = goalValue;
+    }
+    public void setCurrentWeight(double currentWeight){
+        this.currentWeight = currentWeight;
+    }
+    public String getGoalType(){
+        return goalType;
+    }
+    public double getValue(){
+        return goalValue;
+    }
+    public double getCurentWeight(){
+        return currentWeight;
+    }
+    @Override
+    public void update(){
+
+    }
+
+    @Override
+    public void setDate(LocalDate date) {
+
+    }
+
+    @Override
+    public LocalDate getDate() {
+        return null;
+    }
+    public void record(){
+
+    }
 }
