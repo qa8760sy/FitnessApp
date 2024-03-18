55beba27a19486d74ebf5d47b55c8e416300f421
bpunyagunr <132117860+bpunyagunr@users.noreply.github.com>
2024-03-18 16:36:34 -0500

    UserProfile.java

--- src/main/java/oop/trials/fittracker/user/UserProfile.java
+++ src/main/java/oop/trials/fittracker/user/UserProfile.java
@@ -1,2 +1,45 @@
-package oop.trials.fittracker.user;public class UserProfile {
+package oop.trials.fittracker.user;
+
+public class UserProfile {
+    private int age;
+    private float height, weight;
+    private Goal goal;
+
+    public UserProfile(){
+
+    }
+    public UserProfile(UserAccount userAccount){
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public void setGoal(Goal goal) {
+        this.goal = goal;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public Goal getGoal() {
+        return goal;
+    }
 }
