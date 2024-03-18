9556ed05e300148e3b894f7065d62e3f271f365f
bpunyagunr <132117860+bpunyagunr@users.noreply.github.com>
2024-03-18 16:36:15 -0500

    UserAccount.java

--- src/main/java/oop/trials/fittracker/user/UserAccount.java
+++ src/main/java/oop/trials/fittracker/user/UserAccount.java
@@ -1,2 +1,46 @@
-package oop.trials.fittracker.user;public class UserAccount {
+package oop.trials.fittracker.user;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class UserAccount {
+    private String username;
+    private String password;
+    private String email;
+    private static final String EMAIL_REGEX = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+
+    public UserAccount(String username, String password, String email){
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public UserAccount(){}
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
+    public void setEmail(String email) throws IllegalArgumentException {
+        if (validate(email)) {
+            this.email = email;
+        } else {
+            throw new IllegalArgumentException("Invalid email format. Please re-enter your email.");
+        }
+    }
+    public String getUsername(){
+        return username;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public boolean validate(String email){
+        Pattern pattern = Pattern.compile(EMAIL_REGEX);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
 }
