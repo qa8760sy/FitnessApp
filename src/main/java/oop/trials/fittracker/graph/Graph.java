8dee65b47e6096997b3219be143c0d6232cfca8c
bpunyagunr <132117860+bpunyagunr@users.noreply.github.com>
2024-03-18 16:35:03 -0500

    Graph.java

--- src/main/java/oop/trials/fittracker/graph/Graph.java
+++ src/main/java/oop/trials/fittracker/graph/Graph.java
@@ -1,2 +1,25 @@
-package oop.trials.fittracker.graph;public class Graph {
+package oop.trials.fittracker.graph;import java.time.LocalDate;
+public class Graph{
+    private LocalDate date;
+    private Workout workout;
+    private Goal goal;
+
+    public void setDate(LocalDate date){
+        this.date = date;
+    }
+    public void setWorkout(Workout workout){
+        this.workout = workout;
+    }
+    public void setGoal(Goal goal){
+        this.goal = goal;
+    }
+    public LocalDate getDate(){
+        return date;
+    }
+    public Workout getWorkout(){
+        return workout;
+    }
+    public Goal goal(){
+        return goal;
+    }
 }
