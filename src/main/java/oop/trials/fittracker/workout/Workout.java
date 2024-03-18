3e1117f6fd4c975df7e57b352dd3f84115eb54aa
bpunyagunr <132117860+bpunyagunr@users.noreply.github.com>
2024-03-18 16:38:11 -0500

    Workout.java

--- src/main/java/oop/trials/fittracker/workout/Workout.java
+++ src/main/java/oop/trials/fittracker/workout/Workout.java
@@ -1,2 +1,41 @@
-package oop.trials.fittracker.workout;public class WOrkout {
-}
+package oop.trials.fittracker.workout;
+
+import java.time.LocalDate;
+
+public abstract class Workout implements Record{
+    private int amountOfTime;
+    private double caloriesBurned;
+    private int workoutIntensity;
+
+    public void showCaloriesBurned() {
+        System.out.println("You've burned "+caloriesBurned);
+    }
+    public void showInput(double input){
+        System.out.println("You've entered " + input);
+    }
+
+    public void setCaloriesBurned(double caloriesBurned){
+        this.caloriesBurned = caloriesBurned;
+    }
+    public abstract void calculateBurntCalories();
+
+    public void rateWorkout(int intensity) {
+        this.workoutIntensity = intensity;
+    }
+    @Override
+    public void update(){
+
+    }
+
+    @Override
+    public void setDate(LocalDate date) {
+
+    }
+
+    @Override
+    public LocalDate getDate() {
+        return null;
+    }
+    public void record(){
+
+    }
