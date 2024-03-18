b6613591c63da8d008454642e0e4adabafbdd265
bpunyagunr <132117860+bpunyagunr@users.noreply.github.com>
2024-03-18 16:35:49 -0500

    Tracker.java

--- src/main/java/oop/trials/fittracker/tracker/Tracker.java
+++ src/main/java/oop/trials/fittracker/tracker/Tracker.java
@@ -1,2 +1,28 @@
-package oop.trials.fittracker.tracker;public class Tracker {
+package oop.trials.fittracker.tracker;
+public class Tracker {
+    private UserAccount userAccount;
+    private UserProfile userProfile;
+    private Goal userGoal;
+    private Workout workout;
+    private Graph graph;
+
+    public void setUserAccount(UserAccount userAccount) {this.userAccount = userAccount;}
+
+    public void setUserProfile(UserProfile userProfile) {this.userProfile = userProfile;}
+
+    public void setUserGoal(Goal userGoal) {this.userGoal = userGoal;}
+
+    public void setWorkout(Workout workout) {this.workout = workout;}
+
+    public void setGraph(Graph graph) {this.graph = graph;}
+
+    public UserAccount getUserAccount() {return userAccount;}
+
+    public UserProfile getUserProfile() {return userProfile;}
+
+    public Goal getUserGoal() {return userGoal;}
+
+    public Workout getWorkout() {return workout;}
+
+    public Graph getGraph() {return graph;}
 }
